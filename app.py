e9512dc633372428285519204d79855c56bfbda2
BHAVSARRAHUL02 <rbhavsar2002@gmail.com>
2023-08-14 11:21:36 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import newrelic.agent
+
+from flask import Flask, request
+from flask import render_template
+
+newrelic.agent.initialize('newrelic.ini')
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form.get('todo')
+    todos.append(todo)
+    return render_template('index.html', todos=todos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
